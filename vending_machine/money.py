b05d2ac673f538eab776a6b3d75bb6533217bce2
atsushimemet <noap3b69n@gmail.com>
2021-09-18 12:58:04 +0900

    [fix] Money

--- vending_machine/money.py
+++ vending_machine/money.py
@@ -1,11 +1,19 @@
-# class M_10:
-#     def __init__(self, amount):
-#         self.amount = amount
+from enum import Enum, unique
 
 
-class Money:
+@unique
+class Money(Enum):
+
+    M_1 = 1
+    M_5 = 5
+    M_10 = 10
+    M_50 = 50
+    M_100 = 100
+    M_500 = 500
+    M_1000 = 1000
+    M_2000 = 2000
+    M_5000 = 5000
+    M_10000 = 10000
+
     def __init__(self, amount):
         self.amount = amount
-
-
-M_10 = Money(10)
