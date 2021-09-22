79ff6bf70ba97cefb8ea96b2f275e77948ba637c
LM1398 <leomatsu13@gmail.com>
2021-09-22 18:39:17 +0900

    [add] adding money box and change to vending machine

--- tests/vending_machine/test_vending_machine.py
+++ tests/vending_machine/test_vending_machine.py
@@ -2,29 +2,25 @@ from vending_machine.money import Money
 from vending_machine.vending_machine import VendingMachine
 import pytest
 
-vending_machine = VendingMachine()
-
 
 @pytest.mark.parametrize(
     "cash, change",
     [
         (Money.M_1, [Money.M_1]),
-        (Money.M_5, [Money.M_1, Money.M_5]),
-        (Money.M_10, [Money.M_1, Money.M_5]),
-        (Money.M_50, [Money.M_1, Money.M_5]),
-        (Money.M_100, [Money.M_1, Money.M_5]),
-        (Money.M_500, [Money.M_1, Money.M_5]),
-        (Money.M_1000, [Money.M_1, Money.M_5]),
-        (Money.M_2000, [Money.M_1, Money.M_5, Money.M_2000]),
-        (Money.M_10000, [Money.M_1, Money.M_5, Money.M_2000, Money.M_10000]),
+        (Money.M_5, [Money.M_5]),
+        (Money.M_10, []),
+        (Money.M_50, []),
+        (Money.M_100, []),
+        (Money.M_500, []),
+        (Money.M_1000, []),
+        (Money.M_2000, [Money.M_2000]),
+        (Money.M_10000, [Money.M_10000]),
     ],
 )
 def test_change(cash, change):
+    vending_machine = VendingMachine()
     vending_machine.insert(cash)
     assert vending_machine.change == change
-
-
-vending_machine = VendingMachine()
 
 
 @pytest.mark.parametrize(
@@ -33,24 +29,16 @@ vending_machine = VendingMachine()
         (Money.M_1, []),
         (Money.M_5, []),
         (Money.M_10, [Money.M_10]),
-        (Money.M_50, [Money.M_10, Money.M_50]),
-        (Money.M_100, [Money.M_10, Money.M_50, Money.M_100]),
-        (Money.M_500, [Money.M_10, Money.M_50, Money.M_100, Money.M_500]),
-        (
-            Money.M_1000,
-            [Money.M_10, Money.M_50, Money.M_100, Money.M_500, Money.M_1000],
-        ),
-        (
-            Money.M_2000,
-            [Money.M_10, Money.M_50, Money.M_100, Money.M_500, Money.M_1000],
-        ),
-        (
-            Money.M_10000,
-            [Money.M_10, Money.M_50, Money.M_100, Money.M_500, Money.M_1000],
-        ),
+        (Money.M_50, [Money.M_50]),
+        (Money.M_100, [Money.M_100]),
+        (Money.M_500, [Money.M_500]),
+        (Money.M_1000, [Money.M_1000],),
+        (Money.M_2000, [],),
+        (Money.M_10000, [],),
     ],
 )
 def test_money_box(cash, money_box):
+    vending_machine = VendingMachine()
     vending_machine.insert(cash)
 
     assert vending_machine.money_box == money_box
