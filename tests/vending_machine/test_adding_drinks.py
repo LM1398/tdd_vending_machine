41666250b1eb3fee6172b467fd81a2d9e29bd467
LM1398 <leomatsu13@gmail.com>
2021-09-22 20:08:01 +0900

    [fix] removing other asserts since if vending_machine.stock == Counter({coke: 5}) is true, the rest is also true

--- tests/vending_machine/test_adding_drinks.py
+++ tests/vending_machine/test_adding_drinks.py
@@ -7,8 +7,5 @@ def testing_drink():
     vending_machine = VendingMachine()
     coke = Drinks("coke", 120)
     vending_machine.add_drink(coke, 5)
-    assert coke.name == "coke"
-    assert coke.price == 120
-    assert vending_machine.fridge == ["coke", "coke", "coke", "coke", "coke"]
     assert vending_machine.stock == Counter({"coke": 5})
 
