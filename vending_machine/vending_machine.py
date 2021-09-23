d1f60b4e73b8cdc9cf1db98cf85ff39f2a449853
LM1398 <leomatsu13@gmail.com>
2021-09-23 13:16:28 +0900

    [add] docstrings to class, each method and attribute

--- vending_machine/vending_machine.py
+++ vending_machine/vending_machine.py
@@ -1,6 +1,4 @@
-    """ Contains money_box,change, fridge, and stock to organize money and drinks.
-insert method: inserts money from money.py into change and money_box.
-add_drink: adds drink from drink.py into fridge and stock.
+    """ Contains class VendingMachine which is used to organize and group the other src files. 
     """
 
 from vending_machine.money import Money
@@ -10,17 +8,29 @@ from collections import Counter
 
 class VendingMachine:
     """ Main class to organize all of the different src files and classes.
-    __init__ is used to create instances to insert the drink and money instances from the other classes (money.py and drinks.py)
-    insert separates the types of money into accepted and not-accepted(change) and inserts them into the different lists
-    add_drink is used to add a certain amount of drinks into the fridge list, and the stock counts the number of each drink
     """
     def __init__(self):
+        """Used to create instances to insert the drink and money instances from the other classes (money.py and drinks.py).
+        """
         self.money_box = []
+        """list where the approved types of currencies are inserted
+        """
         self.change = []
+        """list where the unaccepted types of currencies are inserted (in future will also include change from buying drinks)
+        """
         self.fridge = []
+        """list with the name of each drink that is inserted in the machine"
+        """
         self.stock = {}
+        """dictionary that counts the amount of each drink in self.fridge"
+        """
 
     def insert(cash):
+        """ insert separates the types of money into accepted and not-accepted(change) and inserts them into the different lists.
+
+        Args:
+            cash (instance): Money.M_x instance from class Money in money.py
+        """
         accepted = [
             Money.M_10,
             Money.M_50,
@@ -34,6 +44,12 @@ class VendingMachine:
             VendingMachine.change.append(cash)
 
     def add_drink(self, drink, amount):
+        """add_drink is used to add a certain amount of drinks into the fridge list, and the stock counts the number of each drink.
+
+        Args:
+            drink (instance): instance of specific drink created using class Drink in drink.py
+            amount (int): desired number of drinks to be added
+        """
         for x in range(amount + 1):
             if x < amount:
                 self.fridge.append(drink.name)
