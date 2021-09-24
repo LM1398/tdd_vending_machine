4b2c922454e8cfa3073c33e026c1480158327422
LM1398 <leomatsu13@gmail.com>
2021-09-24 17:45:31 +0900

    [fix] rewriting docstrings in google style

--- vending_machine/vending_machine.py
+++ vending_machine/vending_machine.py
@@ -7,29 +7,29 @@ from collections import Counter
 
 
 class VendingMachine:
-    """ Main class to organize all of the different src files and classes.
+    """ Main VendingMachine class to organize all of the different src files and classes.
+    
+    Attributes:
+        money_box = A list where the approved types of currencies are inserted.
+        change = A list where the unaccepted types of currencies as well as change from buying drinks are inserted.
+        fridge = A list with the name of each drink that is inserted in the machine.
+        stock = A dictionary that counts the amount of each drink in self.fridge.
+
     """
 
     def __init__(self):
-        """Used to create instances to insert the drink and money instances from the other classes (money.py and drinks.py).
+        """Inits VendingMachine with moneybox, change, fridge, and stock.
         """
         self.money_box = []
-        """list where the approved types of currencies are inserted
-        """
         self.change = []
-        """list where the unaccepted types of currencies are inserted (in future will also include change from buying drinks)
-        """
         self.fridge = []
-        """list with the name of each drink that is inserted in the machine"
-        """
         self.stock = {}
-        """dictionary that counts the amount of each drink in self.fridge"
-        """
 
     def insert(self, cash):
-        """ insert separates the types of money into accepted and not-accepted(change) and inserts them into the different lists.
+        """ Inserts money separates the types of money into accepted and not-accepted(change) and inserts them into the different lists.
+
         Args:
-            cash (instance): Money.M_x instance from class Money in money.py
+            cash (Money): Money.M_x instance
         """
         accepted = [
             Money.M_10,
@@ -46,8 +46,8 @@ class VendingMachine:
     def add_drink(self, drink, amount):
         """add_drink is used to add a certain amount of drinks into the fridge list, and the stock counts the number of each drink.
         Args:
-            drink (instance): instance of specific drink created using class Drink in drink.py
-            amount (int): desired number of drinks to be added
+            drink (Drinks): instance of specific drink.
+            amount (int): desired number of drinks to be added.
         """
         for _drink in range(1, amount + 1):
             if _drink <= amount:
@@ -55,13 +55,13 @@ class VendingMachine:
         self.stock = Counter(self.fridge)
 
     def purchasable(self, drink):
-        """Returns whether a drink is purchasable depending on how much money is inserted and whether the drink is in fridge
+        """Returns whether a drink is purchasable depending on how much money is inserted and whether the drink is in the fridge.
 
         Args:
-            drink ([instance]): instance of specific drink created using class Drink in drink.py
+            drink (Drinks): instance of specific drink
 
         Returns:
-            [string]: "drink is purchasable" or "drink is not in stock"
+            str: "drink is purchasable" or "drink is not in stock"
         """
         total = 0
         for cash in self.money_box:
